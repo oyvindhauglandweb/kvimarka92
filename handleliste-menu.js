8aedc29478f2518a58321647703c2c01e900aa70
oyvindhauglandweb <oyvind.haugland@outlook.com>
2026-06-18 00:07:55 +0200

    Add files via upload

--- handleliste-menu.js
+++ handleliste-menu.js
@@ -2,7 +2,7 @@
 /* Handleliste felles toppmeny v3
    Produksjonsnavn: handleliste-menu.js */
 (function(){
-  const MENU_VERSION = "handleliste-menu-v5-2026-06-17";
+  const MENU_VERSION = "handleliste-menu-v7-2026-06-17";
 
   function svg(name){
     const common = 'viewBox="0 0 24 24" aria-hidden="true" focusable="false"';
@@ -355,6 +355,7 @@
           ${settingsLink("handleliste-varsling.html", "Varsling")}
           ${settingsLink("handleliste-varetyper.html", "Varetyper")}
           ${settingsLink("handleliste-rediger.html", "Varer")}
+          ${settingsLink("handleliste-historikk.html", "Historikk")}
           ${settingsLink("handleliste-rydde.html", "Rydde i varer")}
           ${settingsLink("handleliste-oppskrifter-rediger.html", "Oppskrifter")}
           ${actionLink("sendShoppingListEmail", "E-post")}
